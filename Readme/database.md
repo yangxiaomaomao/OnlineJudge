67b9f7311b83a9f1c0faafba3669a7ae64492abc
Yang Xiaomao <1261181617@qq.com>
2023-09-06 14:58:21 +0800

    add-pack-and-del-archive-script

--- Readme/database.md
+++ Readme/database.md
@@ -1,3 +1,10 @@
 1. 直接使用docker，不需要另外安装。其中，
 2. mysql版本为5.7，用来保存学生的数据
 3. redis用来保存验证码（登录时候需要验证码）
+4. 学生信息在ucas_mysql容器中保存
+    1. `docker exec -it ucas_mysql /bin/bash`进入容器
+    2. `mysql -u root -p`后根据提示输入密码`2020Cnic@!`后进入mysql
+    3. `show databases;`展示数据库有哪些，使用`use oj;`选择`oj`数据库 
+    4. `show tables;`显示`oj`数据库中有哪些表
+    5. `select * from User;`，打印用户信息
+    6. `delete from User where userId>=300`删除用户信息，lyg和yjb的编号远小于300
---
+++ pack_del_archive.py
@@ -0,0 +1,35 @@
+import os
+import sys
+
+## del archive of the last term's students
+## !!ATTENTION, make sure the path string is not none, or your system will be down
+def del_archive():
+    print("Pay attention to the cmd you will execute")
+    sys.exit(0)
+    dir_list = [str(i) for i in range(13,25)]
+    for dir in dir_list:
+        hw_path = os.path.join("/archive", dir)
+        assert(hw_path != "")
+        assert(hw_path != " ")
+        os.system("rm -rf %s/*" % hw_path)
+    
+
+## pack archives at the end of a term
+def pack_archive():
+    dir_name = "/archive"
+    home_path = "/home/yangxiaomao"
+    os.system("sudo cp -r %s %s" % (dir_name,home_path))
+    os.system("sudo chmod 777 -R %s/archive")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python pack_del_archive.py OPERATION, OPERATION supports \"del\" or \"pack\"")
+        sys.exit(0)
+        
+    if sys.argv[1] == "del":
+        del_archive()
+    elif sys.argv[1] == "pack":
+        pack_archive()
+    else:
+        print("Only pack and del is supported")
+        sys.exit(0)
